0faed226d1a7b5a0da3cbbc1589e9212285a039a
TheFifthFox <anthonydyvoon@gmail.com>
2024-09-24 16:31:39 +0200

    Update Gin

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ const ginebras = [
         descripcion: "Una ginebra escocesa conocida por su infusión de pepino y pétalos de rosa, dándole un toque suave y floral. Ideal para un gin-tonic refrescante.",
         notas: "Floral, pepino, rosa, enebro sutil",
         pais: "Escocia",
-        grados: "41,4º"
+        grados: "41,4"
     },
     {
         nombre: "Bombay Sapphire",
         descripcion: "Destilada con 10 botánicos exóticos, esta ginebra tiene un sabor equilibrado, suave y limpio. Su botella azul es icónica y se asocia a cócteles elegantes.",
         notas: "Cítricos, enebro, especias suaves",
         pais: "Reino Unido",
-        grados: "43º"
+        grados: "43"
     },
     {
         nombre: "Tanqueray London Dry",
         descripcion: "Una de las ginebras más clásicas y apreciadas del mundo. Con un fuerte sabor a enebro y un final seco, es la favorita para cócteles tradicionales.",
         notas: "Enebro intenso, pimienta, cilantro",
         pais: "Reino Unido",
-        grados: "41,4º",
+        grados: "41,4",
     },
     {
-        nombre: "Monkey 47",
-        descripcion: "Proveniente de la Selva Negra alemana, esta ginebra utiliza 47 botánicos, creando una ginebra rica, compleja y aromática.",
-        notas: "Frutas cítricas, especias, notas herbales",
-        pais: "Alemania",
-        grados: "41,4º",
+        nombre: "Brockmans Gin",
+        descripcion: "Con el proceso de destilación en la que las notas más tradicionales de la ginebra se combinan exquisitamente con la suave y equilibrada armonía creada por los arándanos azules y las moras",
+        notas: "Frutos del bosque, enebro suave, cítrico",
+        pais: "Inglaterra",
+        grados: "40",
     },
     {
         nombre: "Beefeater London Dry",
@@ -36,11 +36,11 @@ const ginebras = [
         grados: "41,4º",
     },
     {
-        nombre: "The Botanist",
-        descripcion: "Destilada en la isla escocesa de Islay, utiliza 22 hierbas locales que le dan un carácter único. Es una ginebra artesanal y muy fresca.",
-        notas: "Herbal, mentolado, botánicos frescos",
-        pais: "Escocia",
-        grados: "41,4º"
+        nombre: "Rives Gin Special",
+        descripcion: "Ginebra elaborada siguiendo el método London Dry, que se elabora en alambiques de cobre del artesano John Dore.",
+        notas: "Coriandro, piel de limón, piel de naranja amarga, raiz de angélica",
+        pais: "España",
+        grados: "40"
     }
 ];
 
